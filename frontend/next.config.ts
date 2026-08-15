1888dad80d564de50f9bbd04de0dccb479ad0b4a
Stefan Furrer <stefan-furrer@hotmail.com>
2026-08-15 17:50:07 +0200

    Configure standalone mode and add Dockerfile for frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone" // skip unnecessary node modules
 };
 
 export default nextConfig;
